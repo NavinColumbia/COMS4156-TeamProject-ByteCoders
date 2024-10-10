de108731bc9ddecc55d076a05fed3f36adc3ebd4
orlicohen <107129422+orlicohen@users.noreply.github.com>
2024-10-09 22:05:37 -0400

    Add files via upload

---
+++ pharmaid/src/main/java/coms4156/bytecoders/pharmaid/PharmaidApplication.java
@@ -0,0 +1,13 @@
+package coms4156.bytecoders.pharmaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaidApplication.class, args);
+	}
+
+}
---
+++ pharmaid/src/test/java/coms4156/bytecoders/pharmaid/PharmaidApplicationTests.java
@@ -0,0 +1,13 @@
+package coms4156.bytecoders.pharmaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
