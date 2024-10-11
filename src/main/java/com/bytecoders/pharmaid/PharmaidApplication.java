0e3939e25d9ceff8524a18c42688e38e3f8386b8
Orli Cohen <oec2109@barnard.edu>
2024-10-11 16:06:20 -0400

    Chore: adding initial files to team repo

---
+++ src/main/java/com/bytecoders/pharmaid/PharmaidApplication.java
@@ -0,0 +1,13 @@
+package com.bytecoders.pharmaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaidApplication.class, args);
+	}
+
+}
