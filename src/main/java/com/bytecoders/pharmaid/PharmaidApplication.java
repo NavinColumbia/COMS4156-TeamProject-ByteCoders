38142d3d07194ceed9eb5a4a7faeb99b6b4631ac
Orli Cohen <oec2109@barnard.edu>
2024-10-14 22:45:28 -0400

    added simple hello endpoint for testing and fixed checkstyle warnings

--- src/main/java/com/bytecoders/pharmaid/PharmaidApplication.java
+++ src/main/java/com/bytecoders/pharmaid/PharmaidApplication.java
@@ -3,11 +3,15 @@ package com.bytecoders.pharmaid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Class contains all the startup logic for the application.
+ *
+ */
 @SpringBootApplication
 public class PharmaidApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PharmaidApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(PharmaidApplication.class, args);
+  }
 
 }
--- src/main/java/com/bytecoders/pharmaid/PharmaidController.java
+++ src/main/java/com/bytecoders/pharmaid/PharmaidController.java
@@ -1,8 +1,22 @@
 package com.bytecoders.pharmaid;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;  
 
+/**
+ * This class contains all the API routes for the system.
+ *
+ */
 @RestController  
 public class PharmaidController {
     
+  /**
+   * Basic hello endpoint for testing. 
+   *
+   * @return A String 
+   */
+  @GetMapping({ "/", "/hello"})
+  public String index() {
+    return "Hello :)";
+  }
 }
