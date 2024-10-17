8ad0f2926e8dde6f837c7f72180a46b7c628653c
Zach Cox <zach.s.cox@gmail.com>
2024-10-17 11:14:01 -0400

    Add data annotation to prescriptions

--- src/main/java/com/bytecoders/pharmaid/repository/model/Prescription.java
+++ src/main/java/com/bytecoders/pharmaid/repository/model/Prescription.java
@@ -6,9 +6,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.Date;
+import lombok.Data;
 import org.hibernate.annotations.UuidGenerator;
 
 
@@ -16,6 +16,7 @@ import org.hibernate.annotations.UuidGenerator;
  * Model class to describe the "prescriptions" table.
  */
 @Entity
+@Data
 @Table(name = "prescriptions")
 public class Prescription {
 
